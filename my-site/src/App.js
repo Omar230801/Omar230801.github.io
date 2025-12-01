498a8ce40e070faa6fd291e4294432b1478eabfa
Omar230801 <o.ndoye@studenti.unibs.it>
2025-12-01 10:45:32 +0100

    Update App.js

--- my-site/src/App.js
+++ my-site/src/App.js
@@ -1,9 +1,9 @@
 import React from "react";
-import Navbar from "../components/Navbar";
-import About from "../components/About";
-import Projects from "../components/Projects";
-import CV from "../components/CV";
-import Footer from "../components/Footer";
+import Navbar from "./components/Navbar";
+import About from "./components/About";
+import Projects from "./components/Projects";
+import CV from "./components/CV";
+import Footer from "./components/Footer";
 
 function App() {
   return (
